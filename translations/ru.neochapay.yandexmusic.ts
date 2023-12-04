7600020aceef4ecd995d6af46e7282209b2fab29
Sergey Chupligin <neochapay@gmail.com>
2023-12-04 13:49:36 +0300

    [App] Fixup icon and name

---
+++ translations/ru.neochapay.yandexmusic.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FeedPage</name>
+    <message>
+        <location filename="../qml/pages/FeedPage.qml" line="51"/>
+        <source>Oops. We have a problem.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedPage.qml" line="60"/>
+        <source>You feed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FeedPage.qml" line="87"/>
+        <source>Collect for you</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistPage</name>
+    <message>
+        <location filename="../qml/pages/PlaylistPage.qml" line="15"/>
+        <source>Current tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="19"/>
+        <source>Search audio...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>YandexMusic</name>
+    <message>
+        <location filename="../qml/YandexMusic.qml" line="62"/>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/YandexMusic.qml" line="63"/>
+        <source>tags...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
