e860c7a25c56b3a3dfad0dbb53f15e424280ecd1
Sergey Chupligin <neochapay@gmail.com>
2023-12-05 12:40:28 +0300

    [App] Cleanup main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,39 +14,19 @@
 
 #include "models/currentplaylistmodel.h"
 
-#include <QGuiApplication>
-#include <QQmlContext>
-#include <QQuickView>
-#include <QScopedPointer>
-#include <QStandardPaths>
-#include <QtGlobal>
 #include <sailfishapp.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-    // SailfishApp::main() will display "qml/YandexMusic.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-
     QScopedPointer<QGuiApplication> application(SailfishApp::application(argc, argv));
     application->setOrganizationName(QStringLiteral("ru.neochapay"));
     application->setApplicationName(QStringLiteral("yandexmusic"));
 
     QScopedPointer<QQuickView> view(SailfishApp::createView());
 
-    qRegisterMetaType<Album>("Album");
     qRegisterMetaType<Artist>("Artist");
     qRegisterMetaType<Cover>("Cover");
     qRegisterMetaType<Playlist>("Playlist");
-    qRegisterMetaType<Track>("Track");
     qRegisterMetaType<DerivedColors>("DerivedColors");
 
     qmlRegisterType<Track>();
